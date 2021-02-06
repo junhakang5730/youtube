71788dfd51d0526360afb0aa5b50de7d296f2a15
junhakang <junhakang5730@gmail.com>
2021-02-06 19:13:10 +0900

    search function

--- src/app.jsx
+++ src/app.jsx
@@ -5,7 +5,9 @@ import MainList from "./components/mainList";
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const [searchingKeyWord, setSearchingKeyWord] = useState("");
 
+  // 최초 목록 불러옴
   useEffect(() => {
     const requestOptions = {
       method: "GET",
@@ -21,10 +23,32 @@ function App() {
       .catch((error) => console.log("error", error));
   }, []);
 
+  // 새로 검색
+  // useEffect(() => {
+  //   const requestOptions = {
+  //     method: "GET",
+  //     redirect: "follow",
+  //   };
+
+  //   fetch(
+  //     `https://youtube.googleapis.com/youtube/v3/search?q=${searchingKeyWord}&part=snippet&maxResults=3&key=AIzaSyDc3Z-GOTObkz8h3Rcyk4wtB9f-Lgp7_kU`,
+  //     requestOptions
+  //   )
+  //     .then((response) => response.json())
+  //     .then((result) => setVideos(result.items))
+  //     .catch((error) => console.log("error", error));
+  // }, [searchingKeyWord]);
+
+  function searchingNewKeyword(keyword) {
+    // setSearchingKeyWord(keyword);
+    console.log("seaching ", keyword);
+  }
+
   return (
     <div className="container">
-      <Nav />
-      <MainList videos={videos} />
+      <Nav searching={searchingNewKeyword} />
+      <span>{searchingNewKeyword}</span>
+      <MainList videos={videos !== undefined ? videos : []} />
     </div>
   );
 }
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,12 +1,32 @@
-import React, { memo } from "react";
+import React, { memo, useState, useEffect } from "react";
 import "./nav.css";
 
-const Nav = memo(() => {
+const Nav = memo(({ searching }) => {
+  const [searchWord, setSearchWord] = useState("");
+
+  const onClick = () => {
+    searching(searchWord);
+  };
+
+  const onKeyDown = (e) => {
+    if (e.key == "Enter") {
+      console.log("Enter!");
+    }
+  };
+
+  useEffect(() => {
+    console.log(searchWord);
+  }, [searchWord]);
+
+  function handleChange(e) {
+    setSearchWord(e.target.value);
+  }
+
   return (
     <nav>
       <img className="Logo" src="./Logo.svg" alt="" />
-      <input type="text" />
-      <button>Search</button>
+      <input type="search" onChange={handleChange} onKeyDown={onKeyDown} />
+      <button onClick={onClick}>Search</button>
     </nav>
   );
 });
